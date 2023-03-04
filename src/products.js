46415587f7fd4b6ac60f8206bc396ad9e5a594cb
Aidana777 <aidananiazalieva27@gmail.com>
2023-03-04 23:45:42 +0600

    basket

---
+++ src/products.js
@@ -0,0 +1,50 @@
+export const PRODUCTS = [
+    {
+      id: 1,
+      productName: "Ноутбук Acer Nitro 5 AN515-45 R585SGN (NH.QB9ER.004)",
+      price: 999.0,
+      productImage:' https://object.pscloud.io/cms/cms/Photo/img_0_62_2767_0_1.webp',
+    },
+    {
+      id: 2,
+      productName: "Ноутбук Acer Nitro 5 AN515-58 (NH.QFJER.002)",
+      price: 1999.0,
+      productImage: 'https://object.pscloud.io/cms/cms/Photo/img_0_62_2678_8_6.webp',
+    },
+    {
+      id: 3,
+      productName: " Ноутбук Asus X515EA-BQ3269 (90NB0TY1-M038L0) ",
+      price: 699.0,
+      productImage:' https://object.pscloud.io/cms/cms/Photo/img_0_62_2724_0_1_320.webp',
+    },
+    {
+      id: 4,
+      productName: "WLS Van Gogh Denim Jacket",
+      price: 228.0,
+      productImage: 'https://object.pscloud.io/cms/cms/Photo/img_0_77_4249_8_6_320.webp',
+    },
+    {
+      id: 5,
+      productName: "Ноутбук Lenovo Legion 5 Pro 16ARH7H-R7165SGN (82RG00GURK)",
+      price: 19.99,
+      productImage: 'https://object.pscloud.io/cms/cms/Photo/img_0_62_2773_7_1.webp',
+    },
+    {
+      id: 6,
+      productName: "Ноутбук Asus ROG Strix G513 (90NR0502-M00050)",
+      price: 68.0,
+      productImage: 'https://object.pscloud.io/cms/cms/Photo/img_0_62_2362_6_1_160.webp',
+    },
+    {
+      id: 7,
+      productName: "AUTO SERVICE SHIRT by GOLF WANG",
+      price: 120.0,
+      productImage: 'https://object.pscloud.io/cms/cms/Photo/img_0_62_2588_1_1.webp',
+    },
+    {
+      id: 8,
+      productName: "Ноутбук Acer Extensa 15 EX215-52 (NX.EG8ER.021)",
+      price: 40.0,
+      productImage: 'https://object.pscloud.io/cms/cms/Photo/img_0_62_2570_7_6.webp',
+    },
+  ];
